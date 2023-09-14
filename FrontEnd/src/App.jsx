fd1d04842a68224630eeda554612e5c215213078
Miro Saarinen <mirosaa@metropolia.fi>
2023-09-14 13:00:11 +0300

    added login and signup page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/pages/home";
+import Home from "./components/pages/Login";
 
 function App() {
   return (
