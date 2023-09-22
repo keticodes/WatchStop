095a94b08ae8d0b93427bf0440a18f595cfb53cd
AkseliKaarre <akselias02gmail.com>
2023-09-22 13:47:50 +0300

    added sidebar

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,18 +1,44 @@
+import React, {useState} from 'react';
 import { Link, useLocation } from "react-router-dom";
 import "../components/css/navbar.css";
+import { SideMenu } from './Sidemenu'
+import "../components/css/sidemenu.css"
 
 const Navbar = () => {
   const location = useLocation();
   const isLoginPage = location.pathname === "/login";
   const isRegisterPage = location.pathname === "/signup";
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => {
+    console.log("Hamburger button clicked");
+    setSidebar(!sidebar);
+  };
 
   return (
     <div className="Navbar-wrapper">
       <div className="Navbar-hamburger">
-        <button>
+        <button onClick={showSidebar}>
           <img src="./hamburger.svg" height="40px" width="40px" alt="" />
         </button>
       </div>
+      <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+            <ul className='nav-menu-items' onClick={showSidebar}>
+              <li className='navbar-toggle'>
+                <Link to ='#' className='menu-bars'>
+                </Link>
+              </li>
+              {SideMenu.map((item,index) => {
+                return (
+                  <li key={index} className={item.cName}>
+                    <Link to={item.path}>
+                      <span>{item.title}</span>
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </nav>
       <div className="Navbar-title">
         <Link to="/">
           <h1>WATCHSTOP</h1>
---
+++ FrontEnd/src/components/Sidemenu.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+ export const SideMenu = [
+    {
+        title: 'Home',
+        path: '/',
+        cName: 'nav-text'
+    },
+    {
+        title: 'Watches',
+        path: '/watches',
+        cName: 'nav-text'
+    },
+    {
+        title: 'About us',
+        path: '/about',
+        cName: 'nav-text'
+    },
+]
+
