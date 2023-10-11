d15649d666e9e5fa0a61616c942e51f214095645
Jesper <88264190+SplinteeX@users.noreply.github.com>
2023-10-11 12:57:56 +0300

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -8,4 +8,10 @@ const server = http.createServer(app);
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
   console.log(`Server running on port ${config.PORT}`);
+
+  const swaggerHost = config.HOST || "localhost"; // Default to "localhost" if HOST is not specified in config
+  const swaggerPort = config.PORT || 5173; // Default to 5173 if PORT is not specified in config
+  const swaggerRoute = `http://${swaggerHost}:${swaggerPort}/api-docs`;
+
+  logger.info(`Swagger documentation available at: ${swaggerRoute}`);
 });
