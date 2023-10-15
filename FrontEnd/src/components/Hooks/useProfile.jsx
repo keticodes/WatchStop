28b55afa3388e3e4d4a3c92cb06beb7f7e39447a
Jesper <88264190+SplinteeX@users.noreply.github.com>
2023-10-15 20:14:18 +0300

    Hooks back again

---
+++ FrontEnd/src/components/Hooks/useProfile.jsx
@@ -0,0 +1,26 @@
+import Cookies from "js-cookie";
+
+const useProfile = async () => {
+  const token = Cookies.get("token");
+
+  if (!token) {
+    throw new Error("User token not found");
+  }
+  const apiUrl = "http://localhost:3001/api/users/profile";
+  try {
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    const userData = await response.json();
+    return userData;
+  } catch (error) {
+    throw new Error(`Error fetching user profile: ${error.message}`);
+  }
+};
+
+export default useProfile;
