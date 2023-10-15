c1fdc503ac0bee8bedc64dad5188bd62a4a10778
Jesper <88264190+SplinteeX@users.noreply.github.com>
2023-10-15 20:13:21 +0300

    Delete FrontEnd/src/components/Hooks directory

--- FrontEnd/src/components/Hooks/useLogout.jsx
+++
@@ -1,7 +0,0 @@
-import Cookies from "js-cookie";
-const useLogout = () => {
-  Cookies.remove("token");
-  Cookies.remove("userId");
-  console.log("Logout successful");
-};
-export default useLogout;
