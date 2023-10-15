533890762b987ba8936934a8d2bd268b14748940
Jesper <88264190+SplinteeX@users.noreply.github.com>
2023-10-15 22:45:07 +0300

    Authorization to token

--- BackEnd/helpers/createToken.js
+++ BackEnd/helpers/createToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const createToken = (id, res) => {
   const token = jwt.sign({ userId: id }, process.env.JWT_SECRET);
   if (res) {
-    res.cookie("Authorization", token, {
+    res.cookie("token", token, {
       httpOnly: true,
       expires: new Date(Date.now() + 3600000),
     });
